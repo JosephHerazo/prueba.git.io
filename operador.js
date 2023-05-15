3fcb417120ac6df5756129592aff3193b68fec70
JosephHerazo <131680117+JosephHerazo@users.noreply.github.com>
2023-05-15 09:34:15 -0500

    main

--- operador.js
+++ operador.js
@@ -1,5 +1,10 @@
-document.write("Problema 3 <br>");
-var celsius = prompt("coloque los grados celsius");
-var n1 = parseInt(celsius);
-var result = 9/5 * n1 + 32;
-document.write(result);
+//Calcular la edad de la persona con el año del nacimiento:
+ //deberan preguntar en que ano nacio
+ //Calcular la edad en base del año y el ano actual
+
+ var year1 = prompt("En que año naciò"); 
+ var actualyear = prompt("Cuàl es el año actual");
+ var a = parseint(year1);
+ var b =  parseint(actualyear);
+ var result = b-a;
+ document.write(result); 
